8b0d7ad7d7df9f47c8527d9cbd7110930ef8d8e9
Rikin Katyal <katyalrikin@gmail.com>
2015-09-19 06:26:45 -0400

    test

---
+++ hghjk/hghjk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hghjk
+//
+//  Created by Rikin Katyal on 2015-09-19.
+//  Copyright (c) 2015 Rikin Katyal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
